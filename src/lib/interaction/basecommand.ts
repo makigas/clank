b66e65d847d8d83a127c8dc5cfeff839f50694c9
Dani Rodríguez <dani@danirod.es>
2021-07-07 20:11:22 +0200

    refactor: extract sendResponse to avoid instantiating fake command classes

--- src/lib/interaction/basecommand.ts
+++ src/lib/interaction/basecommand.ts
@@ -1,15 +1,7 @@
-import axios from "axios";
 import { APIGuildInteraction } from "discord-api-types";
 import { Guild } from "discord.js";
 import Makibot from "../../Makibot";
-import logger from "../logger";
-
-const interactionsClient = axios.create({
-  baseURL: "https://discord.com/api/v8",
-  headers: {
-    "Content-Type": "application/json",
-  },
-});
+import { sendResponse } from "../interaction";
 
 export default abstract class InteractionCommand<Params> {
   protected readonly client: Makibot;
@@ -24,17 +16,6 @@ export default abstract class InteractionCommand<Params> {
   abstract handle(guild: Guild, params?: Params): Promise<void>;
 
   sendResponse(response: string, ephemeral: boolean = false): Promise<void> {
-    const payload: any = {
-      type: 4,
-      data: { content: response },
-    };
-    if (ephemeral) {
-      payload.data.flags = 64;
-    }
-    logger.debug("[interactions] sending response: ", payload);
-    return interactionsClient.post(
-      `/interactions/${this.event.id}/${this.event.token}/callback`,
-      payload
-    );
+    return sendResponse(this.event, response, ephemeral);
   }
 }
